87efd6469492b3b78a483b790c5aaeae7738f185
wchstrife <wch960426@126.com>
2019-11-12 23:18:56 +0800

    带人脸检测的双边滤波

--- beautyskin.py
+++ beautyskin.py
@@ -8,28 +8,32 @@ def distance(x1, y1, x2, y2):
     return numpy.sqrt(numpy.abs((x1-x2)**2 - (y1-y2)**2))
 
 def bilateral_filter(image, diameter, sigma_i, sigma_s, face_pos):
-    new_image = numpy.zeros(image.shape)
+    #new_image = numpy.zeros(image.shape)
+    new_image = image
 
-    for chanel in range(3):
-        for row in range(len(image)):
-            for col in range(len(image[0])):
-                wp_total = 0            # 模板的总权值
-                filtered_image = 0      # 中心点的权值
-                for k in range(diameter):
-                    for l in range(diameter):
-                        n_x = row - (diameter/2 - k)
-                        n_y = col - (diameter/2 - l)
-                        if n_x >= len(image):
-                            n_x -= len(image)
-                        if n_y >= len(image[0]):
-                            n_y -= len(image[0])
-                        gi = gaussian(image[int(n_x)][int(n_y)][chanel] - image[row][col][chanel], sigma_i)
-                        gs = gaussian(distance(n_x, n_y, row, col), sigma_s)
-                        wp = gi * gs
-                        filtered_image = (filtered_image) + (image[int(n_x)][int(n_y)][chanel] * wp)
-                        wp_total = wp_total + wp
-                filtered_image = filtered_image // wp_total
-                new_image[row][col][chanel] = int(numpy.round(filtered_image))
+    for (x, y, w, h) in face_pos: # 对图像中每个人脸做双边滤波
+        for chanel in range(3):
+            for row in range(x, x + w):
+                for col in range(y, y + h):
+                    wp_total = 0            # 模板的总权值
+                    filtered_image = 0      # 中心点的权值
+                    for k in range(diameter):
+                        for l in range(diameter):
+                            n_x = row - (k - diameter/2)
+                            n_y = col - (l - diameter/2)
+                            # if n_x >= len(image):
+                            #     n_x -= len(image)
+                            # if n_y >= len(image[0]):
+                            #     n_y -= len(image[0])
+                            if n_x < len(image)  and n_x > 0 and  n_y < len(image[0]) and n_y > 0 : # 忽略边缘
+                                gi = gaussian(image[int(n_x)][int(n_y)][chanel] - image[row][col][chanel], sigma_i)
+                                gs = gaussian(distance(n_x, n_y, row, col), sigma_s)
+                                wp = gi * gs
+                                filtered_image = (filtered_image) + (image[int(n_x)][int(n_y)][chanel] * wp)
+                                wp_total = wp_total + wp
+                    filtered_image = filtered_image // wp_total
+                    new_image[row][col][chanel] = int(numpy.round(filtered_image))
+
     return new_image
 
 # 检测人脸
@@ -41,21 +45,31 @@ def detect(filename):
     # 进行人脸检测
     # 返回一个列表，列表里边每一项是一个框起人脸的矩形(x, y, w, h)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    print(faces)
     
     return faces
 
 
 if __name__ == "__main__":
-    filename = "./0171_01.jpg"
+    filename = "./lena512color.tiff"
     image = cv2.imread(filename, 1) # 读取图像
-    face_pos = detect(filename)     # 检测人脸位置
+    image_old = image
 
-    print(face_pos)
+    face_pos = detect(filename)    # 检测人脸位置
 
     cv2.imshow("original image", image) # 显示原始图像
+    cv2.imwrite("oldimage.jpg", image)
 
-    image_new = bilateral_filter(image, 7, 20.0, 20.0, face_pos)
-    
+    image_new = bilateral_filter(image_old, 7, 20.0, 20.0, face_pos)
+
+    # 给人脸加框
+    for x, y, w, h in face_pos:
+        image_new = cv2.rectangle(image_new, (x, y), (x+w, y+h), (255, 0, 0), 2)  
+
+    cv2.imshow("process image", image_new) # 显示处理后图像
+    cv2.imwrite("newimage.jpg", image_new)
+
+    cv2.waitKey()
 
     # filtered_image_OpenCV = cv2.bilateralFilter(image, 7, 20.0, 20.0)
     # cv2.imwrite("filtered_image_OpenCV.png", filtered_image_OpenCV)
